b99ec68e05285505c6ff066fe7f4a5f69484d03b
FWGTJ <115330379+FWGTJ@users.noreply.github.com>
2024-06-05 16:13:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def communicate():
         messages=messages
     )
 
-    bot_message = response["choices"][0]["message"]["content"]
+    bot_message = response.choices[0].message["content"]
     messages.append({"role": "assistant", "content": bot_message})
 
     st.session_state["user_input"] = ""  # 入力欄を消去
